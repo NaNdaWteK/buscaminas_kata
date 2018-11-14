4826d98fbfc9f8052eaf1a85575be011ea40b238
nandawteK <nandawtek@gmail.com>
2018-11-14 23:20:41 +0100

    [GREEN] During exporation I sense that has not needed test

--- spec/buscaminas_spec.rb
+++ spec/buscaminas_spec.rb
@@ -1,31 +1,4 @@
 describe 'Game' do
-  context 'Cell' do
-    it 'knows it has a bomb near its position' do
-      x_bomb = 0
-      y_bomb = 0
-      x_cell = 0
-      y_cell = 1
-      bomb = Bomb.new(x_bomb, y_bomb)
-      game = Game.new
-      game.put_bombs(bomb)
-      cell = Cell.new(x_cell, y_cell)
-
-      expect(cell.bombs?).to be true
-    end
-
-    it 'knows it has not a bomb near its position' do
-      x_bomb = 0
-      y_bomb = 0
-      x_cell = 2
-      y_cell = 3
-      bomb = Bomb.new(x_bomb, y_bomb)
-      game = Game.new
-      game.put_bombs(bomb)
-      cell = Cell.new(x_cell, y_cell)
-
-      expect(cell.bombs?).to be false
-    end
-  end
   context 'Game' do
     it 'game cell know how many bombs has near it' do
       x_bomb = 0
@@ -53,11 +26,6 @@ class Cell
   def initialize(x_position, y_position)
     @x_position = x_position
     @y_position = y_position
-  end
-
-  def bombs?
-    return true if @x_position == 0
-    return false
   end
 end
 
