9bad668a60b47c907666288e7c6c8eda67262fc4
nandawteK <nandawtek@gmail.com>
2018-11-14 23:14:46 +0100

    [RED]Test game cell kwows how much bombs has

--- spec/buscaminas_spec.rb
+++ spec/buscaminas_spec.rb
@@ -26,6 +26,20 @@ describe 'Game' do
       expect(cell.bombs?).to be false
     end
   end
+  context 'Game' do
+    it 'game cell know how many bombs has near it' do
+      x_bomb = 0
+      y_bomb = 0
+      x_cell = 0
+      y_cell = 1
+      bomb = Bomb.new(x_bomb, y_bomb)
+      game = Game.new
+      game.put_bombs(bomb)
+      cell = Cell.new(x_cell, y_cell)
+
+      expect(game.how_much_bombs(cell)).to eq 1
+    end
+  end
 end
 
 class Bomb
@@ -51,7 +65,11 @@ class Game
   def initialize
     @bombs = []
   end
+
   def put_bombs(bomb)
     @bombs << bomb
+  end
+
+  def how_much_bombs(cell)
   end
 end
